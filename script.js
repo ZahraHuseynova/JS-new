d1de07b45e6f5860263ab453749a6b2e186ab0a0
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-08-09 12:58:37 +0400

    first js

---
+++ script.js
@@ -0,0 +1,19 @@
+let steak = confirm("do you like steak?")
+if (steak) {
+    document.write("Here's a sandwich steak")
+
+} else {
+    document.write ("Here’s a Tofu Stir-Fry!")
+}
+
+let age = prompt("What is your birth year?")
+if(age<1997) {
+    alert("Here's your sake")
+}else if (age>1997) {
+    alert("no sake for you")
+} else {
+    alert("Here you are")
+}
+
+
+ 
